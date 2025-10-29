2474a4e29c4e0a5e8379bf2ec8b3b846ee6da9d1
Akshar Rawal <aksharrawal@Akshars-MacBook-Pro.local>
2025-10-29 20:02:48 +0530

    fix linter error

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    // Exclude Supabase Edge Functions from webpack build
-    config.externals = [...(config.externals || []), { 'std/http/server': 'std/http/server' }];
-    return config;
-  },
+  experimental: {},
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  // Exclude Supabase Edge Functions from build process
+  distDir: 'dist',
+  output: 'standalone'
 };
 
 export default nextConfig;
